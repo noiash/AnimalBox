0beee0c32991269ea6d31bf309c85cdb2158898f
noiash <noinoi51267@gmail.com>
2021-12-03 08:12:04 +0000

    [Edit]Rspec User

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
-describe 'ユーザー新規登録' do
-  context '新規登録できる場合' do
-    it '新規ユーザー登録画面に遷移する' do
-      visit root_path
-      click_button ('新規会員登録はこちら')
-      expect(page).to have_current_path '/users/sign_up'
+RSpec.describe 'Users', type: :system do
+  let(:user) { create(:user) }
+  describe 'ログイン前' do
+    describe 'ユーザー新規登録' do
+      context 'フォームの入力値が正常' do
+        it '会員登録が成功する' do
+          visit new_user_registration_path
+          fill_in 'user[email]', with: 'test@test'
+          fill_in 'user[password]', with: 'password'
+          fill_in 'user[password_confirmation]', with: 'password'
+          click_button 'Sign up'
+          expect(current_path).to eq('/posts')
+        end
+      end
     end
   end
 end
