32eac1a95ef8a560a6e9534616bfe12e01eff3b9
noiash <noinoi51267@gmail.com>
2021-12-14 02:02:34 +0000

    [Finish]ペットコントローラー

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,6 +18,9 @@ class PetsController < ApplicationController
 
   def edit
     @pet = Pet.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
